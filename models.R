9e969cc1c6f4277c556faf8349d68b5eb3ec5390
j-cqln <94996390+j-cqln@users.noreply.github.com>
2024-08-18 17:30:59 -0400

    Data cleaning, model update

--- models.R
+++ models.R
@@ -6,7 +6,7 @@ library(ModelMetrics)
 
 # Process data
 process_data <- function(d, season) {
-  last_event_time_cutoff = 5
+  last_event_time_cutoff = 3
   time_since_last_default = 99
   
   # Shot info
@@ -28,7 +28,9 @@ process_data <- function(d, season) {
            time_since_last = timeSinceLastEvent,
            last_event_team = lastEventTeam,
            shooter = shooterName,
+           shooter_id = shooterPlayerId,
            goalie = goalieNameForShot,
+           goalie_id = goalieIdForShot,
            on_goal = shotWasOnGoal,
            creates_rebound_shot = shotGeneratedRebound,
            rebound_shot = shotRebound,
@@ -69,7 +71,7 @@ process_data <- function(d, season) {
     select(season, season_type, game, event_id, shot_id, period, time,
            team, event, goal_diff, time_since_last, last_event,
            x_coord, y_coord, dist, abs_angle,
-           shooter, goalie, shot_type, angle_change,
+           shooter, shooter_id, goalie, goalie_id, shot_type, angle_change,
            on_goal, goal,
            creates_rebound_shot, rebound_shot,
            frozen, play_stopped, in_zone, out_zone)
@@ -80,8 +82,8 @@ process_data <- function(d, season) {
 # Additional data processing for model use
 process_model_data <- function(d) {
   d$goal_diff <- as.factor(d$goal_diff)
-  d$shooter <- as.factor(d$shooter)
-  d$goalie <- as.factor(d$goalie)
+  d$shooter_id <- as.factor(d$shooter_id)
+  d$goalie_id <- as.factor(d$goalie_id)
   d$shot_type <- as.factor(d$shot_type)
   
   return(d)
@@ -94,9 +96,9 @@ model <- function(x, y) {
   fit <- glmer(y ~ 1 +
                  ns(dist, df = 6) + ns(abs_angle, df = 6) +
                  (1 | goal_diff) +
-                 (1 | shooter) + (1 | goalie) +
+                 (1 | shooter_id) + (1 | goalie_id) +
                  (1 | shot_type),
-               data = x, family = binomial())
+               data = x, family = binomial(), nAGQ = 0)
   
   return(fit)
 }
@@ -120,24 +122,52 @@ for (i in 1:length(data_files)) {
   } else {
     data <- rbind(data, d)
   }
+  
+  rm(d)
 }
 
-rm(d)
+# Group by season/game/period, create rebound goal
+data <- data %>%
+  group_by(season, game, period) %>%
+  mutate(rebound_goal = ifelse(team == lead(team) &
+                                 creates_rebound_shot == 1 &
+                                 lead(rebound_shot) == 1 &
+                                 lead(goal) == 1,
+                                1, 0))
 
 # Save data
 write.csv(data, "data/processed_shots.csv", row.names = FALSE)
 
-# Build model
+# Build models
 model_data <- process_model_data(data)
+model_data <- head(model_data, 40000)
+
 xg_model <- model(model_data, model_data$goal)
+model_data$xg <- predict(xg_model, model_data,
+                         type = "response", allow.new.levels = TRUE)
 
-# Save model
+xrebounds_model <- model(model_data, model_data$creates_rebound_shot)
+model_data$xrebounds <- predict(xrebounds_model, model_data,
+                                type = "response", allow.new.levels = TRUE)
+
+xrebound_value_model <- model(model_data, model_data$rebound_goal)
+model_data$xrebound_value <- predict(xrebound_value_model, model_data,
+                                     type = "response", allow.new.levels = TRUE)
+
+# Save models
 saveRDS(xg_model, "models/xg_model.rds")
+saveRDS(xrebounds_model, "models/xrebounds_model.rds")
+saveRDS(xrebound_value_model, "models/xrebound_value_model.rds")
 
-# Predictions
-model_data$xg <- predict(xg_model, model_data, type = "response")
-
-# Group by game and period and assign xG of next shot if it generates rebound
+# Group by shooter ID and compute total rebound xG and average rebound xG
 model_data <- model_data %>%
-  group_by(game, period) %>%
-  mutate(rebound = ifelse(team == lead(team) & creates_rebound_shot == 1  & lead(last_event) == "SHOT", lead(xg), NA))
+  group_by(season, shooter_id) %>%
+  mutate(total_xg = sum(xg, na.rm = TRUE),
+         avg_xg = mean(xg, na.rm = TRUE),
+         total_xrebounds = sum(xrebounds, na.rm = TRUE),
+         avg_xrebounds = mean(xrebounds, na.rm = TRUE),
+         total_rebound_value = sum(xrebound_value, na.rm = TRUE),
+         avg_rebound_value = mean(xrebound_value, na.rm = TRUE))
+
+# Save model data
+write.csv(model_data, "data/model_data.csv", row.names = FALSE)
